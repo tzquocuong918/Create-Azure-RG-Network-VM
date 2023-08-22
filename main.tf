de7d44c91e7d5722c9d5685444d6742110379745
tzquocuong918 <72614483+tzquocuong918@users.noreply.github.com>
2023-08-22 12:15:23 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,76 @@
+terraform {  
+  required_providers {  
+    azurerm = {  
+      source = "hashicorp/azurerm"  
+      version = ">=2.96.0"
+    }  
+  }  
+}  
+  
+provider "azurerm" {  
+    version = ">= 2.96.0"
+    subscription_id   = "3f87c349-6fa2-455b-bd5b-xxxxxx"
+    tenant_id         = "037f8e6b-2776-49f6-bd0f-xxxxxx"
+    client_id         = "17d948e8-5a27-499f-a64c-xxxxxx"
+    client_secret     = "kSe8Q~aH.~E4En0gmX2NxQ13_7xxxx" 
+  features {}  
+}  
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
